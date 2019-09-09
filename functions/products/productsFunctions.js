823a29c1698dd611fea65eac61abd5aff78ea9cc
Cesar Vega <vegac0189@gmail.com>
2019-09-09 01:12:16 -0300

    Agrego DELETE y SEARCH de un producto

--- functions/products/productsFunctions.js
+++ functions/products/productsFunctions.js
@@ -32,9 +32,60 @@ exports.createProduct = functions.https.onRequest((req, res) => {
   const snapshot = db
     .collection('/products')
     .add({
-      name: dataToInsert.name, 
-      id: dataToInsert.id
+      name: data.name, 
+      id: data.id
       });
+
+  res.redirect(303, snapshot.ref.toString());
+  res.end();
+});
+
+exports.productById = functions.https.onRequest((req,res) => {
+  var dni = req.query.dni;
+
+  db.collection('/products')
+  .limit(10)
+  .get()
+  // eslint-disable-next-line promise/always-return
+  .then( querySnapshot => {
+    let usuarios = [];
+    querySnapshot.forEach(doc => {
+      if (doc.data().dni === dni) {
+        usuarios.push(doc.id,doc.data());
+      }
+    });
+    
+    res.send(usuarios);
+    res.end();
+  }).catch(error => {
+    console.log('ERROOOORR', error);
+});
+});
+
+exports.deleteProduct = functions.https.onRequest((req,res) => {
+
+  let dni = req.query.dni;
+  
+  // eslint-disable-next-line promise/catch-or-return
+  db.collection('/products')
+  .get()
+  // eslint-disable-next-line promise/always-return
+  .then(doc => {
+    let flag = [{      
+        edo:false      
+    }];
+    doc.forEach(doc=>{
+      if (doc.data().dni === dni) {
+        flag.edo = true; //confirmo que se elimino
+        db.collection('/usuarios').doc(doc.id).delete();        
+      }
+    });
+
+    res.send(flag.edo); //Si es verdadero fue eliminado.
+  })
+  .catch(err =>{
+    res.send('err=',err);
+  });
 
   res.redirect(303, snapshot.ref.toString());
   res.end();
